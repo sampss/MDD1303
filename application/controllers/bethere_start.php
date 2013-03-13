426886b7deed26c4e2eddb399ab6cce5f91d3bc8
Shawn Sampson <waglenuts@gmail.com>
2013-03-13 01:14:18 -0400

    commit due to untracked files

--- application/controllers/bethere_start.php
+++ application/controllers/bethere_start.php
@@ -1,11 +1,15 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
 class bethere_start extends CI_Controller {
- 
+    
     public function index()
     {
-        echo "Hello World";
-    }
+        
+        $this->load->view('templates/jquery_mobile_header');
+        $this->load->view('pages/jquery_mobile_pages');
+        $this->load->view('templates/jquery_mobile_footer');
+        
+    }// end public function
 }
  
 /* End of file bethere_start.php */
